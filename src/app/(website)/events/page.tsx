6ac70f83cec39edcfd129b92a96cbf1318d57711
Yead191 <yead191@gmail.com>
2026-07-23 14:03:20 +0600

    updated event

--- src/app/(website)/events/page.tsx
+++ src/app/(website)/events/page.tsx
@@ -7,8 +7,13 @@ export default async function page({ searchParams }: { searchParams: Promise<any
 
     const params = new URLSearchParams()
     if (search.q) params.set('searchTerm', search.q)
-    if (search.location) params.set('location', search.location)
-    if (search.date) params.set('date', search.date)
+    if (search.location) params.set('address', search.location)
+    if (search.date) {
+        // search.date is either a single day "2026-07-01" or a range "2026-07-01_2026-07-04"
+        const [startDate, endDate] = search.date.split('_')
+        params.set('startDate', startDate)
+        params.set('endDate', endDate || startDate)
+    }
     if (search.category) params.set('category', search.category)
     params.set('page', search.page || '1')
 
